d57cd58f9dceb896f9b412c59c45333faf528aa9
delta <mika.schlup@gmail.com>
2023-06-27 17:29:44 +0200

    Minor fixes

--- src/main/java/com/mikadev/AnticheatServer.java
+++ src/main/java/com/mikadev/AnticheatServer.java
@@ -42,20 +42,22 @@ public class AnticheatServer implements DedicatedServerModInitializer {
 
         allowedMods = new ArrayList<String>(
                 Arrays.asList(CONFIG.getOrDefault(KEY_ALLOWED_MODS, "").split(",")));
-        allowedMods.addAll(Arrays.asList("fabric-object-builder-api-v1", "java", "fabric-sound-api-v1",
-                "fabric-transitive-access-wideners-v1", "fabric-mining-level-api-v1", "fabric-content-registries-v0",
-                "fabric-item-group-api-v1", "fabric-renderer-indigo", "fabric-transfer-api-v1", "anticheat",
-                "fabric-registry-sync-v0", "fabric-rendering-fluids-v1", "fabric-recipe-api-v1",
-                "fabric-gametest-api-v1", "fabric-resource-conditions-api-v1", "fabric-events-interaction-v0",
-                "fabric-entity-events-v1", "fabric-dimensions-v1", "fabric-block-api-v1", "fabric-client-tags-api-v1",
-                "minecraft", "fabric-api-lookup-api-v1", "fabric-message-api-v1", "fabric-rendering-v1",
-                "fabric-loot-api-v2", "fabric-screen-api-v1", "fabric-networking-api-v1",
-                "fabric-rendering-data-attachment-v1", "fabric-api", "fabricloader", "fabric-lifecycle-events-v1",
-                "fabric-crash-report-info-v1", "fabric-biome-api-v1", "fabric-game-rule-api-v1",
-                "fabric-key-binding-api-v1", "fabric-blockrenderlayer-v1", "fabric-api-base", "fabric-command-api-v2",
-                "fabric-models-v0", "fabric-screen-handler-api-v1", "fabric-particles-v1", "fabric-resource-loader-v0",
-                "fabric-item-api-v1", "fabric-renderer-api-v1", "fabric-data-generation-api-v1",
-                "fabric-convention-tags-v1"));
+        allowedMods.addAll(Arrays.asList("anticheat", "fabric-api", "fabric-api-base",
+                "fabric-api-lookup-api-v1", "fabric-biome-api-v1", "fabric-block-api-v1", "fabric-blockrenderlayer-v1",
+                "fabric-client-tags-api-v1", "fabric-command-api-v1", "fabric-command-api-v2", "fabric-commands-v0",
+                "fabric-containers-v0", "fabric-content-registries-v0", "fabric-convention-tags-v1",
+                "fabric-crash-report-info-v1", "fabric-data-generation-api-v1", "fabric-dimensions-v1",
+                "fabric-entity-events-v1", "fabric-events-interaction-v0", "fabric-events-lifecycle-v0",
+                "fabric-game-rule-api-v1", "fabric-item-api-v1", "fabric-item-group-api-v1",
+                "fabric-key-binding-api-v1", "fabric-keybindings-v0", "fabric-lifecycle-events-v1",
+                "fabric-loot-api-v2", "fabric-loot-tables-v1", "fabric-message-api-v1", "fabric-mining-level-api-v1",
+                "fabric-models-v0", "fabric-networking-api-v1", "fabric-networking-v0", "fabric-object-builder-api-v1",
+                "fabric-particles-v1", "fabric-recipe-api-v1", "fabric-registry-sync-v0", "fabric-renderer-api-v1",
+                "fabric-renderer-indigo", "fabric-renderer-registries-v1", "fabric-rendering-data-attachment-v1",
+                "fabric-rendering-fluids-v1", "fabric-rendering-v0", "fabric-rendering-v1",
+                "fabric-resource-conditions-api-v1", "fabric-resource-loader-v0", "fabric-screen-api-v1",
+                "fabric-screen-handler-api-v1", "fabric-sound-api-v1", "fabric-transfer-api-v1",
+                "fabric-transitive-access-wideners-v1", "fabricloader", "java", "minecraft"));
 
         instantBanResourcePacks = new ArrayList<String>(
                 Arrays.asList(CONFIG.getOrDefault(KEY_INSTANT_BAN_RESOURCE_PACKS, "").split(",")));
@@ -82,14 +84,14 @@ public class AnticheatServer implements DedicatedServerModInitializer {
                         try {
                             String[][] response = ResourcesPacket.readResourcePacket(buf);
                             String[] usedResourcePacks = response[0];
+                            String ipAddress = ((ServerLoginNetworkHandlerAccessor) handler).getConnection()
+                                    .getAddress().toString().split(":")[0].replace("/", "");
 
                             for (String resourcePack : usedResourcePacks) {
                                 if (!allowedResourcePacks.contains(resourcePack)) {
                                     if (instantBanResourcePacks.contains(resourcePack)) {
-                                        BannedIpEntry banEntry = new BannedIpEntry(
-                                                ((ServerLoginNetworkHandlerAccessor) handler).getConnection()
-                                                        .getAddress().toString(),
-                                                null, null, null, "You used highly illegal resource packs.");
+                                        BannedIpEntry banEntry = new BannedIpEntry(ipAddress, null, null, null,
+                                                "You used highly illegal resource packs.");
                                         server.getPlayerManager().getIpBanList().add(banEntry);
 
                                         Anticheat.LOGGER.info("Instant banned player for using " + resourcePack);
@@ -97,7 +99,8 @@ public class AnticheatServer implements DedicatedServerModInitializer {
                                                 Text.literal("You are using highly illegal resource packs."));
                                         return;
                                     } else {
-                                        Anticheat.LOGGER.info("Kicked player for using " + resourcePack);
+                                        Anticheat.LOGGER.info("Kicked player for using " + resourcePack + " ("
+                                                + String.join(", ", usedResourcePacks) + ")");
                                         handler.disconnect(Text.literal("You are using illegal resource packs."));
                                         return;
                                     }
@@ -109,17 +112,16 @@ public class AnticheatServer implements DedicatedServerModInitializer {
                             for (String mod : usedMods) {
                                 if (!allowedMods.contains(mod)) {
                                     if (instantBanMods.contains(mod)) {
-                                        BannedIpEntry banEntry = new BannedIpEntry(
-                                                ((ServerLoginNetworkHandlerAccessor) handler).getConnection()
-                                                        .getAddress().toString(),
-                                                null, null, null, "You used highly illegal mods.");
+                                        BannedIpEntry banEntry = new BannedIpEntry(ipAddress, null, null, null,
+                                                "You used highly illegal modifications.");
                                         server.getPlayerManager().getIpBanList().add(banEntry);
 
                                         Anticheat.LOGGER.info("Instant banned player for using " + mod);
                                         handler.disconnect(Text.literal("You are using highly illegal modifications."));
                                         return;
                                     } else {
-                                        Anticheat.LOGGER.info("Kicked player for using " + mod);
+                                        Anticheat.LOGGER.info("Kicked player for using " + mod + " ("
+                                                + String.join(", ", usedMods) + ")");
                                         handler.disconnect(Text.literal("You are using illegal modifications."));
                                         return;
                                     }
